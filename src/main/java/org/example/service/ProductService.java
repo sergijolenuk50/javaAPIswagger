82f3a900334ec998e72884cd69050b0066e72bcd
sergijolenuk50 <sergijolenuk50@gmail.com>
2025-03-05 19:45:58 +0200

    update delete foto

--- src/main/java/org/example/service/ProductService.java
+++ src/main/java/org/example/service/ProductService.java
@@ -128,7 +128,10 @@ import org.example.repository.IProductImageRepository;
 import org.example.repository.IProductRepository;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -174,51 +177,94 @@ public class ProductService {
     }
 
     public boolean updateProduct(Integer id, ProductPostDto product) {
-        var res = productRepository.findById(id);
-        if (res.isEmpty()) {
-            return false;
-        }
-        var entity = res.get();
+        var entity = productRepository.findById(id).orElseThrow();
+
+        // Оновлення основних даних продукту
         entity.setName(product.getName());
         entity.setDescription(product.getDescription());
         entity.setPrice(product.getPrice());
+
         var cat = new CategoryEntity();
         cat.setId(product.getCategoryId());
         entity.setCategory(cat);
-        productRepository.save(entity);
 
-//        for (var img : product.getRemoveImages()) {
-//            var removeImage = productImageRepository.findByName(img).get();
-//            fileService.remove(img);
-//            productImageRepository.delete(removeImage);
-//        }
+        // Отримуємо список старих зображень у базі
+        Map<String, ProductImageEntity> existingImages = entity.getImages().stream()
+                .collect(Collectors.toMap(ProductImageEntity::getName, img -> img));
 
-        // Перевіряємо наявність і обробляємо видалення зображень
-        if (product.getRemoveImages() != null) {
-            for (var img : product.getRemoveImages()) {
-                var removeImage = productImageRepository.findByName(img);
-                if (removeImage.isPresent()) {
-                    // Видаляємо фото з сервера
-                    fileService.remove(img);
-                    // Видаляємо фото з бази даних
-                    productImageRepository.delete(removeImage.get());
+        List<ProductImageEntity> updatedImages = new ArrayList<>();
+
+        for (int i = 0; i < product.getImages().size(); i++) {
+            var img = product.getImages().get(i);
+
+            if ("old-image".equals(img.getContentType())) {
+                // Оновлення пріоритету старого зображення
+                var imageName = img.getOriginalFilename();
+                if (existingImages.containsKey(imageName)) {
+                    var oldImage = existingImages.get(imageName);
+                    oldImage.setPriority(i);
+                    productImageRepository.save(oldImage);
+                    updatedImages.add(oldImage);
                 }
+            } else {
+                // Додавання нового зображення
+                var imageName = fileService.load(img);
+                var newImage = new ProductImageEntity();
+                newImage.setName(imageName);
+                newImage.setPriority(i);
+                newImage.setProduct(entity);
+                productImageRepository.save(newImage);
             }
         }
-        // Оновлюємо або додаємо нові зображення
-        int priority = 1;
-        for (var img : product.getImages()) {
-            ProductImageEntity image = new ProductImageEntity();
-            var imageName = fileService.load(img);
-            image.setName(imageName);
-            image.setPriority(priority);
-            image.setProduct(entity);
-            productImageRepository.save(image);
-            priority++;
+        List<Integer> removeIds = new ArrayList<>();
+        // Видалення зображень, яких немає в оновленому списку
+        for (var img : entity.getImages()) {
+            if (!updatedImages.contains(img)) {
+                fileService.remove(img.getName());
+                removeIds.add(img.getId());
+            }
         }
 
+        productImageRepository.deleteAllByIdInBatch(removeIds);
         return true;
+    }
 
+
+
+//    public boolean updateProduct(Integer id, ProductPostDto product) {
+//        var res = productRepository.findById(id);
+//        if (res.isEmpty()) {
+//            return false;
+//        }
+//        var entity = res.get();
+//        entity.setName(product.getName());
+//        entity.setDescription(product.getDescription());
+//        entity.setPrice(product.getPrice());
+//        var cat = new CategoryEntity();
+//        cat.setId(product.getCategoryId());
+//        entity.setCategory(cat);
+//        productRepository.save(entity);
+//
+////        for (var img : product.getRemoveImages()) {
+////            var removeImage = productImageRepository.findByName(img).get();
+////            fileService.remove(img);
+////            productImageRepository.delete(removeImage);
+////        }
+//
+//        // Перевіряємо наявність і обробляємо видалення зображень
+//        if (product.getRemoveImages() != null) {
+//            for (var img : product.getRemoveImages()) {
+//                var removeImage = productImageRepository.findByName(img);
+//                if (removeImage.isPresent()) {
+//                    // Видаляємо фото з сервера
+//                    fileService.remove(img);
+//                    // Видаляємо фото з бази даних
+//                    productImageRepository.delete(removeImage.get());
+//                }
+//            }
+//        }
+//        List<ProductImageEntity> updatedImages = new ArrayList<>();
+//        // Оновлюємо або додаємо нові зображення
 //        int priority = 1;
 //        for (var img : product.getImages()) {
 //            ProductImageEntity image = new ProductImageEntity();
@@ -229,9 +275,30 @@ public class ProductService {
 //            productImageRepository.save(image);
 //            priority++;
 //        }
-//
+//        List<Integer> removeIds = new ArrayList<>();
+//        // Видалення зображень, яких немає в оновленому списку
+//        for (var img : entity.getImages()) {
+//            if (!updatedImages.contains(img)) {
+//                fileService.remove(img.getName());
+//                removeIds.add(img.getId());
+//            }
+//        }
+//        productImageRepository.deleteAllByIdInBatch(removeIds);
 //        return true;
-    }
+//
+////        int priority = 1;
+////        for (var img : product.getImages()) {
+////            ProductImageEntity image = new ProductImageEntity();
+////            var imageName = fileService.load(img);
+////            image.setName(imageName);
+////            image.setPriority(priority);
+////            image.setProduct(entity);
+////            productImageRepository.save(image);
+////            priority++;
+////        }
+////
+////        return true;
+//    }
 
     public boolean deleteProduct(Integer id) {
         var res = productRepository.findById(id);
