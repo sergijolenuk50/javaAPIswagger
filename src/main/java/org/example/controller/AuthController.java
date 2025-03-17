467fe61bb46729e7f45c8d21279a7a6c484f52ed
sergijolenuk50 <sergijolenuk50@gmail.com>
2025-03-17 19:11:56 +0200

    додали метод для підтримки googl

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.config.security.JwtService;
 import org.example.dto.user.UserAuthDto;
+import org.example.dto.user.UserGoogleAuthDto;
 import org.example.dto.user.UserRegisterDto;
 
 import org.example.service.UserService;
@@ -51,6 +52,17 @@ public class AuthController {
         try {
             // Перевірка, чи існує користувач і чи правильні дані
             String token = userService.authenticateUser(userEntity);
+            return ResponseEntity.ok("Bearer " + token);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Помилка при вході: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/google")
+    public ResponseEntity<String> google_login(@RequestBody UserGoogleAuthDto userEntity) {
+        try {
+            // Перевірка, чи існує користувач і чи правильні дані
+            String token = userService.signInGoogle(userEntity.getToken());
             return ResponseEntity.ok("Bearer " + token);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Помилка при вході: " + e.getMessage());
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -6,8 +6,21 @@ import org.example.dto.user.UserAuthDto;
 import org.example.dto.user.UserRegisterDto;
 import org.example.entites.UserEntity;
 import org.example.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Map;
+
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +29,9 @@ public class UserService {
     private final IUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+//    записуе змінну
+    @Value("${google.api.userinfo}")
+    private String googleUserInfoUrl;
 
     // Реєстрація нового користувача
 //    public void registerUser(UserEntity userEntity) {
@@ -48,4 +64,29 @@ public class UserService {
         // Генерація JWT токену
         return jwtService.generateAccessToken(foundUser);
     }
+
+    public String signInGoogle(String access_token) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + access_token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(googleUserInfoUrl, HttpMethod.GET, entity, String.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> userInfo = mapper.readValue(response.getBody(), new TypeReference<Map<String, String>>() {
+            });
+            var userEntity = userRepository.findByUsername(userInfo.get("email"))
+                    .orElse(null); // Порожній об'єкт
+            if (userEntity == null) {
+                userEntity = new UserEntity();
+                userEntity.setUsername(userInfo.get("email"));
+                userEntity.setPassword("");
+                userRepository.save(userEntity);
+            }
+            var jwt = jwtService.generateAccessToken(userEntity);
+            return jwt;
+        }
+        return null;
+    }
+
 }
